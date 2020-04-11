cc2953ebfd68328ad0ce5e77c86c59239c0b914a
Lila Tran <litra@microsoft.com>
2020-04-11 00:09:03 -0700

    saving work, hot reload succeeded

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <div>
+        <h1>what you want hello adgadgadg dddddd dsgadagadg's</h1>
+    </div>,
+    document.getElementById("root")
+);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const glob = require("glob");
+
+module.exports = {
+  target: "web",
+  entry: glob.sync("./src/**/*.tsx"),
+  output: {
+    filename: "bundle.js",
+    publicPath: "/dist/"
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    https: false,
+    port: 3000
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  stats: {
+    warnings: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader"
+      },
+      {
+        test: /\.html$/,
+        use: "file-loader"
+      }
+    ]
+  },
+  plugins: [new CopyWebpackPlugin([{ from: "**/*.html", context: "src" }])]
+};
