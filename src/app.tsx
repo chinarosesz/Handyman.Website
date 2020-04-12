eca2e63b144fadcd4e8ec5f0edf271a9e2d86c19
Lila Tran <litra@microsoft.com>
2020-04-11 19:26:51 -0700

    Linting

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,16 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+let a = 1;
+if (a == 1) 
+{
+    console.log("a");
+}
+else
+{
+    console.log('b');
+}
+
 ReactDOM.render(
     <div>
         <h1>Hello World</h1>
